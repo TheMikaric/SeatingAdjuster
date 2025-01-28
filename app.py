525b7329e88f3013c129d7cea529fdb097deea72
TheMikaric <md220088d@student.etf.bg.ac.rs>
2025-01-28 13:17:06 +0100

    Database updates

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+from __future__ import annotations
 from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from typing import List
+
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import relationship
 
 #The app itself
 app = Flask(__name__) # It's named like the Python file
@@ -11,18 +20,18 @@ db = SQLAlchemy(app)
 
 # Class -- row of data
 class MyPerson(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), default='TecMinds',nullable=False)
-    team = db.Column(db.String(20), nullable=False)
-
-    def __repr__(self) -> str:
-        return f'Person with ID of {self.id}, is named {self.name} and is in the team {self.team}'
-
+    __tablename__ = 'Persons'
+    id :  Mapped[int] = mapped_column(primary_key=True)
+    
 with app.app_context():
     db.create_all()
 
+@app.route('/',methods=["POST","GET"]) # Homepage
+def index():
+    return render_template('index.html')
+
 @app.route('/create/person',methods=["POST","GET"]) # Create a person
-def create_a_person():
+def create_person():
     # Add a person to the database
     if request.method == "POST":
         name = request.form['name']
@@ -40,26 +49,21 @@ def create_a_person():
         persons = MyPerson.query.order_by(MyPerson.id).all()
         return render_template('create_person.html', persons=persons)
 
-
-@app.route('/',methods=["POST","GET"]) # Homepage
-def index():
-    return render_template('index.html')
-
 # Delete a person from the database
 @app.route("/delete/person/<int:id>")
-def delete(id:int):
+def delete_person(id:int):
     delete_person = MyPerson.query.get_or_404(id)
     try:
         db.session.delete(delete_person)
         db.session.commit()
-        return redirect("/create_person")
+        return redirect("/create/person")
     except Exception as e:
         print(f'Error: {e}')
         return f'Error: {e}'
 
-#Edit an item
+
 @app.route("/edit/person/<int:id>",methods=["POST","GET"])
-def edit(id:int):
+def edit_person(id:int):
     person = MyPerson.query.get_or_404(id)
     if request.method == "POST":
         person.name = request.form['name']
@@ -72,6 +76,7 @@ def edit(id:int):
     else:
         return render_template('edit_person.html', person=person)
     
+
 if __name__ == "__main__":
 
-    app.run(debug=True) # Debugger is on so Flask updates itself
+    app.run() # Debugger is on so Flask updates itself
